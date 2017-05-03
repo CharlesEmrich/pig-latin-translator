68306b302d27a16366d1d4af296420062483ea6e
Charles Emrich <charles.emrich@gmail.com>
2017-05-03 13:03:07 -0700

    add js to translate whole sentences

--- js/scripts.js
+++ js/scripts.js
@@ -23,19 +23,34 @@ function translateWord(word) {
       }
       newWord = word.slice(moved[1]) + moved[0] + "ay"
     }
+  } else {
+    return word;
   }
   return newWord;
 }
 
 function translateSentence(string) {
-
+var newSentence = "";
+//strip all punctuation and split into words
+var wordArray = string.split(" ");
+// .forEach(function(element) {
+//   element.replace(/\W/g, "");
+// });
+// console.log(wordArray);
+//pass all words through translateWord
+for (var i = 0; i < wordArray.length; i++) {
+  newSentence += translateWord(wordArray[i].toLowerCase()) + " ";
+}
+//repunctuate, recapitalize sentence? Ugh.
+newSentence = newSentence[0].toUpperCase() + newSentence.slice(1);
+return newSentence;
 }
 
 $(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     var userInput = $("textarea[name='input-area']").val();
-    var result = translateWord(userInput);    $("#output").text(result);
+    var result = translateSentence(userInput);    $("#output").text(result);
   });
 });
 
